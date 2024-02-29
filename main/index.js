95d46e8868a6dccd39af7699d1b7ed2ec24cc122
shreeprasaddas <shreepsd2@gmail.com>
2024-02-29 23:07:38 +0545

    update test teacher adding systeym

--- main/index.js
+++ main/index.js
@@ -3,10 +3,15 @@ import login from "../authentication/login.js";
 import register from "../authentication/register.js";
 import token from "../authentication/token.js";
 import express from "express";
-import test from "../test.js";
 import user_record from "../user_database/user_record.js";
+import add_teacher from "../teacher_data/add_teacher.js";
 
-const suggetion = test;
+
+const teacher = add_teacher;
+
+
+
+
 const app=express();
 
 
@@ -41,6 +46,23 @@ app.post("/login",async(req,res)=>{
     }
     res.json(logins);
 });
+
+app.post("/addteacher",async(req,res)=>{
+    
+    try{
+        const {Name,phoneNumb,email,adderess,salry} = req.body;
+        await teacher(Name,phoneNumb,email,adderess,salry);
+        return {
+            error:false
+        }
+    }
+    catch{
+        return{
+            error:true
+        }
+
+    }
+})
 
 
 
